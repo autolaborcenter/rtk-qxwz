7b0c06037bd73716d9bd3b4393c05f8244b42406
YdrMaster <ydrml@hotmail.com>
2021-11-26 16:45:58 +0800

    cleanup: 画图功能整理到一个 feature refactor: 启动千寻位置封装为一个函数，方便调用

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -5,15 +5,18 @@
 };
 use driver::{SupervisorEventForSingle::*, SupervisorForSingle};
 use gnss::{LocalReference, WGS84};
-use monitor_tool::{rgba, vertex, Encoder};
-use nmea::gpfpd::{RtkStatus, Status, SystemStatus};
-use rtk_qxwz::{GpggaSender, RTCMReceiver, RTKBoard, StreamToQXWZ};
+use nmea::NmeaLine::*;
+use rtk_qxwz::{
+    display::{vertex, ENU_TOPIC},
+    monitor_tool::{palette, rgba, Encoder},
+};
+use rtk_qxwz::{prefab, GpggaSender, RTCMReceiver, RTKBoard};
 use std::{f32::consts::FRAC_PI_2, time::Duration};
 
 fn main() {
     let sender: Arc<Mutex<Option<GpggaSender>>> = Arc::new(Mutex::new(None));
     let receiver: Arc<Mutex<Option<RTCMReceiver>>> = Arc::new(Mutex::new(None));
-    spawn_qxwz(sender.clone(), receiver.clone());
+    prefab::spawn_qxwz(sender.clone(), receiver.clone());
 
     const REFERENCE: WGS84 = WGS84 {
         latitude: 39.9926296,
@@ -28,46 +31,42 @@ fn main() {
     SupervisorForSingle::<RTKBoard>::default().join(|e| {
         match e {
             Connected(port, board) => {
-                println!("port = COM{}", port);
+                println!("Port = COM{}", port);
                 *task::block_on(receiver.lock()) = Some(board.get_receiver());
             }
             Disconnected => {
+                println!("Serial disconnected.");
                 *task::block_on(receiver.lock()) = None;
             }
-            Event(_, Some((_, (line, cs)))) => {
-                use nmea::NmeaLine::*;
-                match line {
-                    GPGGA(_, tail) => {
-                        task::block_on(async {
-                            if let Some(ref mut s) = *sender.lock().await {
-                                s.send(tail.as_str(), cs).await;
-                            }
-                        });
-                    }
-                    GPFPD(body) => {
-                        let wgs84 = WGS84 {
-                            latitude: body.latitude as f64 * 1e-7,
-                            longitude: body.longitude as f64 * 1e-7,
-                            altitude: body.altitude as f64 * 1e-2,
-                        };
-                        let enu = reference.wgs84_to_enu(wgs84);
-                        let dir = (body.heading as f32) * 1e-3;
-                        println!(
-                            "{:?} |({}, {})| {:?} | {}",
-                            body.status, body.nsv1, body.nsv2, enu, dir
-                        );
-                        let vertex = vertex!(status_level(body.status); enu.e as f32, enu.n as f32; Arrow, FRAC_PI_2 - dir.to_radians(); 64);
-                        let socket = socket.clone();
-                        task::spawn(async move {
-                            let _ = socket.send(&Encoder::with(|encoder| encoder.topic("enu").push(vertex))).await;
-                        });
-                    }
-                    _ => println!("{:?}", line),
+            Event(_, Some((_, (line, cs)))) => match line {
+                GPGGA(_, tail) => {
+                    task::block_on(async {
+                        if let Some(ref mut s) = *sender.lock().await {
+                            s.send(tail.as_str(), cs).await;
+                        }
+                    });
                 }
-            }
+                GPFPD(body) => {
+                    let wgs84 = WGS84 {
+                        latitude: body.latitude as f64 * 1e-7,
+                        longitude: body.longitude as f64 * 1e-7,
+                        altitude: body.altitude as f64 * 1e-2,
+                    };
+                    let enu = reference.wgs84_to_enu(wgs84);
+                    let dir = (body.heading as f32) * 1e-3;
+                    println!(
+                        "{:?} |({}, {})| {:?} | {}",
+                        body.status, body.nsv1, body.nsv2, enu, dir
+                    );
+                    let vertex = vertex(body.status, enu, FRAC_PI_2 - dir.to_radians());
+                    let packet = Encoder::with(|encoder| encoder.topic(ENU_TOPIC).push(vertex));
+                    let _ = task::block_on(socket.send(&packet));
+                }
+                _ => println!("{:?}", line),
+            },
             Event(_, None) => {}
             ConnectFailed => {
-                println!("serial failed to connect.");
+                println!("Serial failed to connect.");
                 std::thread::sleep(Duration::from_secs(1));
             }
         }
@@ -75,40 +74,12 @@ fn main() {
     });
 }
 
-fn spawn_qxwz(sender: Arc<Mutex<Option<GpggaSender>>>, receiver: Arc<Mutex<Option<RTCMReceiver>>>) {
-    task::spawn_blocking(move || {
-        SupervisorForSingle::<StreamToQXWZ>::default().join(|e| {
-            match e {
-                Connected(key, stream) => {
-                    println!("key = {}", key);
-                    *task::block_on(sender.lock()) = Some(stream.get_sender());
-                }
-                Disconnected => {
-                    *task::block_on(sender.lock()) = None;
-                }
-                Event(_, Some((_, buf))) => {
-                    println!("forward. len = {}", buf.len());
-                    if let Some(ref mut r) = *task::block_on(receiver.lock()) {
-                        r.receive(buf.as_slice());
-                    }
-                }
-                Event(_, None) => {}
-                ConnectFailed => {
-                    println!("network failed to connect.");
-                    std::thread::sleep(Duration::from_secs(1));
-                }
-            }
-            true
-        });
-    });
-}
-
 fn send_config(socket: Arc<UdpSocket>, period: Duration) {
-    let clear = Encoder::with(|encoder| encoder.topic("enu").clear());
+    let clear = Encoder::with(|encoder| encoder.topic(ENU_TOPIC).clear());
     let topic = Encoder::with(|encoder| {
         encoder.config_topic(
-            "enu",
-            50000,
+            ENU_TOPIC,
+            36000,
             500,
             &[
                 (0, rgba!(GRAY; 0.2)),
@@ -130,20 +101,4 @@ fn send_config(socket: Arc<UdpSocket>, period: Duration) {
             task::sleep(period).await;
         }
     });
-}
-
-fn status_level(status: Status) -> u8 {
-    use RtkStatus::*;
-    use SystemStatus::*;
-    let Status(a, b) = status;
-    match (a, b) {
-        (纯惯性, _) => 1,
-        (RTK, Gps1Bd | 双模) => 2,
-        (RTK, RTK浮点解) => 3,
-        (RTK, RTK固定解) => 4,
-        (差分定向, Gps1Bd | 双模) => 5,
-        (差分定向, RTK浮点解) => 6,
-        (差分定向, RTK固定解) => 7,
-        _ => 0,
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,73 @@ mod serial;
 pub use network::{GpggaSender, StreamToQXWZ};
 pub use serial::{RTCMReceiver, RTKBoard};
 
+#[cfg(feature = "display")]
+pub extern crate monitor_tool;
+
+/// 预封装，使用更方便
+pub mod prefab {
+    use super::*;
+    use async_std::{
+        sync::{Arc, Mutex},
+        task,
+    };
+    use driver::{SupervisorEventForSingle::*, SupervisorForSingle};
+    use std::time::Duration;
+
+    const RETRY: Duration = Duration::from_secs(3);
+
+    /// 启动千寻位置服务
+    pub fn spawn_qxwz(
+        sender: Arc<Mutex<Option<GpggaSender>>>,
+        receiver: Arc<Mutex<Option<RTCMReceiver>>>,
+    ) {
+        task::spawn_blocking(move || {
+            SupervisorForSingle::<StreamToQXWZ>::default().join(|e| {
+                match e {
+                    Connected(_, stream) => {
+                        *task::block_on(sender.lock()) = Some(stream.get_sender());
+                    }
+                    Disconnected => {
+                        *task::block_on(sender.lock()) = None;
+                    }
+                    Event(_, Some((_, buf))) => {
+                        if let Some(ref mut receiver) = *task::block_on(receiver.lock()) {
+                            receiver.receive(buf.as_slice());
+                        }
+                    }
+                    Event(_, None) => {}
+                    ConnectFailed => task::block_on(task::sleep(RETRY)),
+                }
+                true
+            });
+        });
+    }
+}
+
+#[cfg(feature = "display")]
+pub mod display {
+    use gnss::Enu;
+    use monitor_tool::{vertex, Vertex};
+    use nmea::gpfpd::{RtkStatus::*, Status, SystemStatus::*};
+
+    pub const ENU_TOPIC: &str = "enu";
+
+    pub fn vertex(status: Status, enu: Enu, dir: f32) -> Vertex {
+        let Status(a, b) = status;
+        let level = match (a, b) {
+            (纯惯性, _) => 1,
+            (RTK, Gps1Bd | 双模) => 2,
+            (RTK, RTK浮点解) => 3,
+            (RTK, RTK固定解) => 4,
+            (差分定向, Gps1Bd | 双模) => 5,
+            (差分定向, RTK浮点解) => 6,
+            (差分定向, RTK固定解) => 7,
+            _ => 0,
+        };
+        vertex!(level; enu.e as f32, enu.n as f32; Arrow, dir; 64)
+    }
+}
+
 // 配置星网宇达
 // use nmea::cmd::Body::*;
 // driver.send(&Set("coordinate,-x,y,-z".into()));
